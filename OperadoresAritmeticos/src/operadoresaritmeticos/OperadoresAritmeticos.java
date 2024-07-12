2bc83d9a9e06ed88ea8d0a9b6b9d2ecce5d06839
Larry-Tenorio <larry.lhtx@gmail.com>
2024-07-12 15:13:23 -0300

    Add files via upload

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author trixti
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       /* int n1 = 4;
+        int n2 = 2;
+        float m = (n1 + n2)/2;
+        System.out.println("A média é igual a: " + m);
+        */
+       
+       /*int numero = 5;
+       numero++;
+       int valor = 5 + numero++; 
+       System.out.println(valor);
+       System.out.println(numero);*/
+       
+       /* int x = 4;
+       x += 2; // x = x + 2 
+       System.out.println(x); 
+       */
+       
+       /*int x = 25;
+       int ra = (int)Math.sqrt(x);
+       System.out.println(ra);*/
+       
+       /*double ale = Math.random();
+       int n = (int)(15 + ale * (50-15));
+       System.out.println(n);*/
+       
+       int v1 = 7, v2 = v1 % 2, v3 = 2;
+       v3 += v2;
+        System.out.println(v1 + "" + v2 + "" + v3);
+    }
+    
+}
